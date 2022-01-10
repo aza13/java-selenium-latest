b3750cd9ffb5c94d5122d146ba803dfcf17956e6
Venkat Kottapalli <vkottapalli@tmhcc.com>
2022-01-10 06:50:56 -0800

    config file updated

--- src/main/java/utils/fileReader/ConfigDataReader.java
+++ src/main/java/utils/fileReader/ConfigDataReader.java
@@ -24,6 +24,8 @@ public class ConfigDataReader {
 
         logger.info("Loading the properties file in :: ConfigPropInit ");
 
+        System.out.println("Loading the properties file in :: ConfigPropInit");
+
         File file = new File(ConstantVariable.CONFIG_PROP_FILEPATH);
 
         if(prop == null){
@@ -39,6 +41,7 @@ public class ConfigDataReader {
                 logger.error("Failed to load the properties file in :: ConfigPropInit ");
             }
         }
+        System.out.println("Returning config prp");
         return prop;
     }
 }
