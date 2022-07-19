2eae1658a7e16d4c999a13fe33f38cf8c293b8eb
Venkat Kottapalli <vkottapalli@tmhcc.com>
2022-07-19 09:46:51 -0700

    Code refactoring in 9.2 release execution

---
+++ src/main/java/workflows/CreateApplicant.java
@@ -0,0 +1,49 @@
+package workflows;
+
+import base.PageObjectManager;
+import constants.ConstantVariable;
+import helper.FakeDataHelper;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import pageActions.DashboardPageActions;
+import pageActions.InsuredPageActions;
+import pageActions.RatingCriteriaPageActions;
+import utils.fileReader.TextFileReader;
+
+public class CreateInsured {
+
+    private static final Logger logger = Logger.getLogger(CreateInsured.class);
+
+    private CreateInsured(){}
+
+    public static String createInsured(WebDriver driver) throws InterruptedException {
+        logger.info("verifying creating new quote creation :: testCreateInsured");
+        DashboardPageActions dashboardPageActions = PageObjectManager.getDashboardPageActions();
+        dashboardPageActions.clickNewQuote(driver);
+        String newInsuredName = FakeDataHelper.fullName();
+        String newInsuredWebsite = FakeDataHelper.website();
+        dashboardPageActions.CreateNewQuote(driver, ConstantVariable.PRODUCT, newInsuredName,newInsuredWebsite);
+        TextFileReader.writeDataToTextFile(ConstantVariable.INSURED_DATA_FILEPATH, newInsuredName+";"+newInsuredWebsite);
+        InsuredPageActions insuredPageActions = dashboardPageActions.clickContinueButton(driver);
+        if (!insuredPageActions.isCreateNewInsuredTextDisplayed(driver)){
+            insuredPageActions.clickNewInsuredButton(driver);
+        }
+        insuredPageActions.enterEmailAddress(driver);
+        insuredPageActions.enterInsuredPhoneNumber(driver);
+        insuredPageActions.enterPhysicalAddress(driver);
+        insuredPageActions.enterPhyCity(driver);
+        insuredPageActions.enterPhyZipcode(driver);
+        insuredPageActions.selectPhyState(driver);
+        insuredPageActions.clickSameAsPhyAddress(driver);
+        insuredPageActions.clickContinueInsuredFormButton(driver);
+        RatingCriteriaPageActions ratingCriteriaPageActions = PageObjectManager.getRatingCriteriaPageActions();
+        if(ratingCriteriaPageActions.isRatingCriteriaPageDisplayed(driver)) {
+            return newInsuredName;
+        }else{
+            return null;
+        }
+
+    }
+
+
+}
