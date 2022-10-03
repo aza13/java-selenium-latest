bbb63fce3005afacc3450b65bf06c528a21cee5a
AUULU <auulu@tmhcc.com>
2022-10-03 07:51:29 -0500

    removed not used imports

--- src/main/java/pageActions/InsuredPageActions.java
+++ src/main/java/pageActions/InsuredPageActions.java
@@ -2,7 +2,6 @@ package pageActions;
 
 import base.BaseTest;
 import helper.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
