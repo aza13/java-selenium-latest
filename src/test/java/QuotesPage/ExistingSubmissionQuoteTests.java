53659d7616cc70614227142dd1790e790d09a280
AUULU <auulu@tmhcc.com>
2022-11-25 23:06:01 -0600

    updated automation for testDownloadApplicationInQuote

--- src/test/java/QuotesPage/ExistingSubmissionQuoteTests.java
+++ src/test/java/QuotesPage/ExistingSubmissionQuoteTests.java
@@ -258,42 +258,6 @@ public class ExistingSubmissionQuoteTests extends BaseTest {
         }
     }
 
-//    @Test(dataProvider = "ask-me", dataProviderClass = TestDataProvider.class, description = "QuotesPageData")
-//    public void testDownloadApplicationInQuote(Map<String, String> map) throws InterruptedException, SQLException {
-//        /***
-//         this test verifies brokers can download application form
-//         This story works with 9.5 only
-//         story - N2020-34254-QAT-434
-//         @author - Azamat Uulu
-//         **/
-//        logger.info("verifying brokers can download application form :: testDownloadApplicationInQuote");
-//        List<HashMap<Object, Object>> submissionIds =
-//                databaseConnector.getResultSetToList(DatabaseQueries.GET_SUBMISSIONS_WITH_CONFIRMED_QUOTES);
-//        int submissionCount = submissionIds.size();
-//        boolean confirmedQuote = false;
-//        String submissionId;
-//        if (submissionCount > 0) {
-//            for (HashMap<Object, Object> id : submissionIds) {
-//                submissionId = id.get("submission_id").toString();
-//                dashboardPageActions.enterTextToSearchBox(DriverManager.getDriver(), submissionId);
-//                if (dashboardPageActions.clickFirstAvailableContinueButton(DriverManager.getDriver())) {
-//                    confirmedQuote = true;
-//                    break;
-//                }
-//                dashboardPageActions.clickClearSearchButton(DriverManager.getDriver());
-//            }
-//            if (confirmedQuote) {
-//                quoteListPageActions.clickQuotesTab(DriverManager.getDriver());
-//                boolean pdfDownload = quoteListPageActions.clickApplicationDownload(DriverManager.getDriver(), map.get("pdfFilename"));
-//                Assert.assertFalse(pdfDownload);
-//
-//            } else {
-//                logger.info("No confirmed quotes available, to download the quote ");
-//            }
-//        }
-//    }
-
-
     @AfterClass(alwaysRun = true)
     public void tearDown() {
         if (databaseConnector != null) {
