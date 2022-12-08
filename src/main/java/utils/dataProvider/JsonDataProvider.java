1214878051963dfe97627e2ffc66a320cc644a66
vkottapalli <vkottapalli@tmhcc.com>
2022-12-08 19:03:35 +0530

    added JsonDataProvider class

---
+++ src/main/java/utils/dataProvider/JsonDataProvider.java
@@ -0,0 +1,12 @@
+package utils.dataProvider;
+
+import org.apache.log4j.Logger;
+
+public class JsonDataProvider {
+
+    private static final Logger logger = Logger.getLogger(JsonDataProvider.class);
+
+    private JsonDataProvider(){}
+
+    // to-do
+}
