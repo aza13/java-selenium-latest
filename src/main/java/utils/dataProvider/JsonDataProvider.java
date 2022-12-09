d75d68e25da811c4d02971bfe0191ac3c52452d1
vkottapalli <vkottapalli@tmhcc.com>
2022-12-09 18:04:01 +0530

    added json data and JsonDataProvider class for login tests

--- src/main/java/utils/dataProvider/JsonDataProvider.java
+++ src/main/java/utils/dataProvider/JsonDataProvider.java
@@ -1,12 +1,58 @@
 package utils.dataProvider;
 
-import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class JsonDataProvider {
 
-    private static final Logger logger = Logger.getLogger(JsonDataProvider.class);
+    private JsonDataProvider() {
+    }
+    @DataProvider(name = "jsonDataReader")
+    public static Object[][] getData(Method method) throws IOException, ParseException {
 
-    private JsonDataProvider(){}
+        String testCaseName = method.getName();
 
-    // to-do
+        Test testAnnotation = method.getAnnotation(Test.class);
+
+        String testClassName = testAnnotation.description();
+
+        String testDataFileName = "./src/main/java/utils/dataProvider/"+testClassName+".json";
+
+        FileReader fileReader = new FileReader(testDataFileName);
+
+        JSONParser jsonParser = new JSONParser();
+
+        Object javaObject = jsonParser.parse(fileReader);
+
+        JSONObject jsonObject = (JSONObject) javaObject;
+
+        JSONArray jsonArray = (JSONArray) jsonObject.get(testCaseName);
+
+        int enabledDataSetsCount = 0;
+
+        for (Object o : jsonArray) {
+            JSONObject testDataSetObject = (JSONObject) o;
+            if (testDataSetObject.get("runMode").equals("Y")) {
+                enabledDataSetsCount += 1;
+            }
+        }
+        Object[][] excelData = new Object[enabledDataSetsCount][1];
+        if(enabledDataSetsCount!=0){
+            for (int i = 0; i < enabledDataSetsCount; i++) {
+                JSONObject testDataSetObject = (JSONObject) jsonArray.get(i);
+                if (testDataSetObject.get("runMode").equals("Y")) {
+                    excelData[i][0]=testDataSetObject;
+                }
+            }
+        }
+        return excelData;
+    }
 }
