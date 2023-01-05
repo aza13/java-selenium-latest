53e361bf328b6b4376acfd8dce29b2aa52dc97f8
AUULU <auulu@tmhcc.com>
2023-01-05 09:02:27 -0600

    updated contact underwriter

--- src/main/java/pageActions/DashboardPageActions.java
+++ src/main/java/pageActions/DashboardPageActions.java
@@ -731,6 +731,8 @@ public class DashboardPageActions extends BaseTest {
 
     public boolean verifyContactUnderwriterExists(WebDriver driver){
         try {
+            String contactUnderwriterText =  driver.findElement(contactUnderwriter).getText();
+            Assert.assertEquals(contactUnderwriterText, "CONTACT UNDERWRITER");
             return ClickHelper.isElementExist(driver, contactUnderwriter);
         }catch (Exception e){
             testLogger.fail("failed to verify the contactUnderwriter :: verifyContactUnderwriterExists" + e.getMessage());
